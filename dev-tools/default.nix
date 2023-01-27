f1563f237e95cd597e9a55f0ea095dfdddd8f6f7
armin-th <armin@eigenfunctor.io>
2023-01-26 19:31:03 -0500

    [ADD] rnix-lsp

--- dev-tools/default.nix
+++ dev-tools/default.nix
@@ -1,8 +1,18 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs, naersk-lib }:
 
 with pkgs;
 
 let
+  rnix = naersk-lib.buildPackage {
+    pname = "rnix-lsp";
+    root = builtins.fetchTarball {
+      url="https://github.com/nix-community/rnix-lsp/archive/refs/tags/v0.2.5.tar.gz";
+      sha256="128h2jih911wvf2pa5g7ryviylapbzvi446xw92qkf8xz3cn6yjr";
+    };
+    doCheck = false;
+    checkInputs = [ pkgs.nix ];
+  };
+
   python = python310;
   pyPkgs = (
     python.withPackages (p: with p; [
@@ -65,12 +75,14 @@ in mkShell rec {
     ipfs
     jq
     jsonnet
+    neovim
     nixfmt
     nodejs
     pkg-config
     protobuf
     pyPkgs
     python
+    rnix
     rust-analyzer
     rustc
     rustfmt
@@ -80,7 +92,6 @@ in mkShell rec {
     templates.dev-shell
     tmux
     vim
-    neovim
     wget
     zig
     zls
--- dev-tools/flake.nix
+++ dev-tools/flake.nix
@@ -2,15 +2,18 @@
   description = "Software development tools";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.naersk.url = "github:nix-community/naersk";
+  inputs.naersk.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
     flake-utils.lib.eachDefaultSystem (
       system: 
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs { inherit system; };
+          naersk-lib = naersk.lib."${system}";
         in {
-          defaultPackage = import ./default.nix { inherit pkgs; };
-          devShells.default = import ./default.nix { inherit pkgs; };
+          defaultPackage = import ./default.nix { inherit pkgs naersk-lib; };
+          devShells.default = import ./default.nix { inherit pkgs naersk-lib; };
         }
     ); 
 }
